d8c5c0f4fab0e3f1f7ef5cb1d269f842fae51de8
Charles-Oscar Combes <charles-oscar.combes@hotmail.com>
2018-03-05 15:13:45 +0100

    Initial Commit

---
+++ Bullify/Models/ViewModels/InfoBoxVM.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Bullify.Models.ViewModels
+{
+    public class InfoBoxVM
+    {
+        public string Description { get; set; }
+        public string Image { get; set; }
+        public string Skillset { get; set; }
+
+    }
+}
